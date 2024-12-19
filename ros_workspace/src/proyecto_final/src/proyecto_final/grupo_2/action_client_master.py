083d80d9bff2d279ca048f1134003eaee4a7a80c
itsasne02 <ipresumido@opendeusto.es>
2024-12-19 14:58:39 +0100

    Update action_client_master.py

--- ros_workspace/src/proyecto_final/src/proyecto_final/grupo_2/action_client_master.py
+++ ros_workspace/src/proyecto_final/src/proyecto_final/grupo_2/action_client_master.py
@@ -1,17 +1,12 @@
 #! /usr/bin/env python
-import sys, rospy, actionlib
+import rospy, actionlib
 # Importación de mensajes ROS
+import numpy as np
 from proyecto_final.msg import FigurasAction, FigurasGoal, FigurasResult
 from proyecto_final.msg import CubosAction, CubosGoal, CubosResult
 from proyecto_final.msg import RLAction, RLGoal, RLResult
+from proyecto_final.funciones_auxiliares import crear_mensaje
 
-# --- CÓDIGO COLORES ---
-c = {
-    "ERROR": "\033[31m",      # Rojo
-    "SUCCESS": "\033[32m",     # Verde
-    "WARN": "\033[33m",  # Amarillo
-    "RESET": "\033[0m"       # Restablecer al color predeterminado
-}
 
 class MasterClient:
     def __init__(self, node_activate:bool=False):
@@ -19,11 +14,15 @@ class MasterClient:
             rospy.init_node('master_client_py')
 
     def obtain_figure(self, order:int=1)->RLResult:
-        name = "FigurasActionServer"
+        name = "FigureMakerActionServer"
         action_client = actionlib.SimpleActionClient(name, FigurasAction)
         goal_msg = FigurasGoal(order=order)
-
-        return self._secuencia_action_client(action_client, name, goal_msg)
+        
+        resultado:FigurasResult = self._secuencia_action_client(action_client, name, goal_msg)
+        if len(resultado.figure_3d) != 0:
+            return np.array(resultado.figure_3d).reshape(resultado.shape_3d)
+        else:
+            return np.array([[[]]])
 
     def obtain_cube_pose(self, goal:int=1):
         name = "CubeTrackerActionServer"
@@ -41,16 +40,16 @@ class MasterClient:
 
     
     def _secuencia_action_client(self, action_client, name, goal_msg):
-        print(f"[INFO] Waiting for {name} server")
+        crear_mensaje(f"Waiting for {name} server", "INFO", "MasterClient")
         action_client.wait_for_server()
 
-        print(f"{c['SUCCESS']}[SUCCESS] Sending goal to {name}{c['RESET']}")
+        crear_mensaje(f"Sending goal to {name}", "SUCCESS", "MasterClient")
         action_client.send_goal(goal_msg)
         
-        print(f"[INFO] Waiting for result from {name}")
+        crear_mensaje(f"Waiting for result from {name}", "INFO", "MasterClient")
         action_client.wait_for_result()
         
-        print(f"{c['SUCCESS']}[SUCCESS] Getting result from {name}{c['RESET']}")
+        crear_mensaje(f"Getting result from {name}", "SUCCESS", "MasterClient")
         return action_client.get_result()
         
 if __name__ == '__main__':
