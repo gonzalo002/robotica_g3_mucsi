6c56eb8aaeb343ef0cf79d6a00e317e5287b6e44
itsasne02 <ipresumido@opendeusto.es>
2024-12-19 11:09:11 +0100

    Update action_client_master.py

--- ros_workspace/src/proyecto_final/src/proyecto_final/grupo_2/action_client_master.py
+++ ros_workspace/src/proyecto_final/src/proyecto_final/grupo_2/action_client_master.py
@@ -1,87 +1,58 @@
 #! /usr/bin/env python
-import sys
-import rospy
+import sys, rospy, actionlib
+# Importación de mensajes ROS
+from proyecto_final.msg import FigurasAction, FigurasGoal, FigurasResult
+from proyecto_final.msg import CubosAction, CubosGoal, CubosResult
+from proyecto_final.msg import RLAction, RLGoal, RLResult
 
-# Brings in the SimpleActionClient
-import actionlib
-
-# Brings in the messages used by the fibonacci action, including the
-# goal message and the result message.
-from proyecto_final.msg import FigurasAction, FigurasActionGoal, FigurasActionResult #Matriz Figura
-from proyecto_final.msg import CubosAction, CubosActionGoal, CubosActionResult #Pose Cubos
-from proyecto_final.msg import RLAction, RLActionGoal, RLActionResult # Orden Cubos
+# --- CÓDIGO COLORES ---
+c = {
+    "ERROR": "\033[31m",      # Rojo
+    "SUCCESS": "\033[32m",     # Verde
+    "WARN": "\033[33m",  # Amarillo
+    "RESET": "\033[0m"       # Restablecer al color predeterminado
+}
 
 class MasterClient:
-    def __init__(self) -> None:
-        # rospy.init_node('master_action_client')
-        pass
+    def __init__(self, node_activate:bool=False):
+        if node_activate:
+            rospy.init_node('master_client_py')
 
-    def client_figura():
-        # Creates the SimpleActionClient, passing the type of the action
-        client = actionlib.SimpleActionClient('figura_to_robot', FigurasActionResult)
+    def obtain_figure(self, order:int=1)->RLResult:
+        name = "FigurasActionServer"
+        action_client = actionlib.SimpleActionClient(name, FigurasAction)
+        goal_msg = FigurasGoal(order=order)
 
-        # Espera hasta que se inicia el servidor y recibes el goal
-        client.wait_for_server()
+        return self._secuencia_action_client(action_client, name, goal_msg)
 
-        # Creates a goal to send to the action server.
-        goal = FigurasActionGoal()
-        goal.goal.order = 1
+    def obtain_cube_pose(self, goal:int=1):
+        name = "CubeTrackerActionServer"
+        action_client = actionlib.SimpleActionClient(name, CubosAction)
+        goal_msg = CubosGoal(order=goal)
+        
+        return self._secuencia_action_client(action_client, name, goal_msg)
 
-        # Sends the goal to the action server.
-        client.send_goal(goal)
+    def obtain_cube_order(self, goal:int=1):
+        name = "RLActionServer"
+        action_client = actionlib.SimpleActionClient(name, RLAction)
+        goal_msg = RLGoal(order=goal)
+        
+        return self._secuencia_action_client(action_client, name, goal_msg)
 
-        # Espera al servidor a finalizar la acción
-        client.wait_for_result()
+    
+    def _secuencia_action_client(self, action_client, name, goal_msg):
+        print(f"[INFO] Waiting for {name} server")
+        action_client.wait_for_server()
 
-        # Muestra el resultado del action ejecutado
-        return client.get_result()  # Reultado de las Figuras
-
-    def client_cubo():
-        # Creates the SimpleActionClient, passing the type of the action
-        client = actionlib.SimpleActionClient('cubo_to_robot', CubosActionResult)
-
-        # Espera hasta que se inicia el servidor y recibes el goal
-        client.wait_for_server()
-
-        # Creates a goal to send to the action server.
-        goal = CubosActionGoal()
-        goal.goal.order = 1
-
-        # Sends the goal to the action server.
-        client.send_goal(goal)
-
-        # Espera al servidor a finalizar la acción
-        client.wait_for_result()
-
-        # Muestra el resultado del action ejecutado
-        return client.get_result()  # Reultado de los Cubos
-
-    def client_rl():
-        # Creates the SimpleActionClient, passing the type of the action
-        client = actionlib.SimpleActionClient('agent_to_robot', RLActionResult)
-
-        # Espera hasta que se inicia el servidor y recibes el goal
-        client.wait_for_server()
-
-        # Creates a goal to send to the action server.
-        goal = RLActionGoal()
-        goal.goal.order = 1
-
-        # Sends the goal to the action server.
-        client.send_goal(goal)
-
-        # Espera al servidor a finalizar la acción
-        client.wait_for_result()
-
-        # Muestra el resultado del action ejecutado
-        return client.get_result()  # Reultado del Agente
-
-    if __name__ == '__main__':
-        try:
-            # Initializes a rospy node so that the SimpleActionClient can
-            # publish and subscribe over ROS.
-            rospy.init_node('fibonacci_client_py')
-            result = fibonacci_client()
-            print("Result:", ', '.join([str(n) for n in result.sequence]))
-        except rospy.ROSInterruptException:
-            print("program interrupted before completion", file=sys.stderr)
+        print(f"{c['SUCCESS']}[SUCCESS] Sending goal to {name}{c['RESET']}")
+        action_client.send_goal(goal_msg)
+        
+        print(f"[INFO] Waiting for result from {name}")
+        action_client.wait_for_result()
+        
+        print(f"{c['SUCCESS']}[SUCCESS] Getting result from {name}{c['RESET']}")
+        return action_client.get_result()
+        
+if __name__ == '__main__':
+    master = MasterClient(True)
+    print(master.obtain_cube_pose())
