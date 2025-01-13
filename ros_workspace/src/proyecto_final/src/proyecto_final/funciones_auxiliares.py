c4666046cbcc22470fa22d4f95821d6b52320883
itsasne02 <ipresumido@opendeusto.es>
2025-01-13 11:38:23 +0100

    Update funciones_auxiliares.py

--- ros_workspace/src/proyecto_final/src/proyecto_final/funciones_auxiliares.py
+++ ros_workspace/src/proyecto_final/src/proyecto_final/funciones_auxiliares.py
@@ -1,12 +1,4 @@
-def crear_mensaje(mensaje:str, tipo:str='ERROR', nombre_clase:str="", tkinter:bool=True) -> str:
-    """
-    Función que crea un mensaje con colores para mostrar en consola.
-        @param mensaje: Mensaje a mostrar.
-        @param tipo: Tipo de mensaje. Puede ser ERROR, SUCCESS, WARN, INFO, INPUT.
-        @param nombre_clase: Nombre de la clase que llama a la función.
-        @param tkinter: Indica si se está ejecutando en un entorno de tkinter.
-        @return: Mensaje con colores.
-    """
+def crear_mensaje(mensaje:str, tipo:str='ERROR', nombre_clase:str="", tkinter:bool=True):
     # --- CÓDIGO COLORES ---
     c = {
         "ERROR":   "\033[31m",  # Rojo
@@ -17,10 +9,11 @@ def crear_mensaje(mensaje:str, tipo:str='ERROR', nombre_clase:str="", tkinter:bo
         "INPUT":   "\033[35m",  # Magenta
     }
 
-    msg = f"{c[tipo]}[{tipo.rjust(7)}] [{nombre_clase.rjust(20)}]: {mensaje} {c['RESET']}"
+    msg_sin_tipo = f"[{tipo.rjust(7)}] [{nombre_clase.rjust(20)}]: {mensaje}"
+    msg = f"{c[tipo]}{msg_sin_tipo}{c['RESET']}"
     if tipo == "INPUT" and not tkinter:
         return input(msg)
     else:
         print(msg)
     
-    return msg
+    return msg_sin_tipo
